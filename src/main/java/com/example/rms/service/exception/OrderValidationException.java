1264a799bc5e68c19a5c5ac24734e3d623790ee4
ahmedibrahimX <a.ibrahim.abdellatif@gmail.com>
2024-10-21 16:47:33 +0300

    refactor(ordering): improve exceptions hierarchy

--- src/main/java/com/example/rms/service/exception/OrderValidationException.java
+++ src/main/java/com/example/rms/service/exception/OrderValidationException.java
@@ -2,10 +2,7 @@ package com.example.rms.service.exception;
 
 import com.example.rms.service.pattern.pipeline.Step;
 
-public class OrderValidationException extends ValidationException {
-
-    public OrderValidationException() {
-    }
+public class OrderValidationException extends Step.StepException implements ValidationException {
 
     public OrderValidationException(String message) {
         super(message);
--- src/main/java/com/example/rms/service/exception/ValidationException.java
+++ src/main/java/com/example/rms/service/exception/ValidationException.java
@@ -1,23 +1,4 @@
 package com.example.rms.service.exception;
 
-public class ValidationException extends CustomException {
-
-    public ValidationException() {
-    }
-
-    public ValidationException(String message) {
-        super(message);
-    }
-
-    public ValidationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public ValidationException(Throwable cause) {
-        super(cause);
-    }
-
-    protected ValidationException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
+public interface ValidationException {
 }
