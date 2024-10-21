e8894d0d41c3fac3b826ea1f748aee3dba1ce720
ahmedibrahimX <a.ibrahim.abdellatif@gmail.com>
2024-10-21 11:52:25 +0300

    deps(cleanup): remove unused dependencies

--- src/main/java/com/example/rms/RmsApplication.java
+++ src/main/java/com/example/rms/RmsApplication.java
@@ -2,9 +2,7 @@ package com.example.rms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.retry.annotation.EnableRetry;
 
-@EnableRetry
 @SpringBootApplication
 public class RmsApplication {
 
