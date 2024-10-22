16385dcd16e7abe7c6f379927c674fef0db5414d
ahmedibrahimX <a.ibrahim.abdellatif@gmail.com>
2024-10-22 19:34:13 +0300

    refactor(ordering): [skip ci] remove unneeded logs

--- src/main/java/com/example/rms/common/exception/handler/GlobalAdvice.java
+++ src/main/java/com/example/rms/common/exception/handler/GlobalAdvice.java
@@ -5,17 +5,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -24,15 +19,11 @@ public class GlobalAdvice {
 
     @ExceptionHandler(value = {InsufficientIngredientsException.class})
     public ResponseEntity<ErrorResponse> handleInsufficientIngredients(InsufficientIngredientsException ex) {
-        log.error(ex.getMessage());
-        log.error(Arrays.toString(ex.getStackTrace()));
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse("InvalidRequest", "Ingredients not available."));
     }
 
     @ExceptionHandler(value = {OrderValidationException.class})
     public ResponseEntity<ErrorResponse> handleOrderValidationFailure(OrderValidationException ex) {
-        log.error(ex.getMessage());
-        log.error(Arrays.toString(ex.getStackTrace()));
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse("InvalidRequest", ex.getMessage()));
     }
 
@@ -44,22 +35,16 @@ public class GlobalAdvice {
 
     @ExceptionHandler(value = {OrderPlacementFailedException.class})
     public ResponseEntity<ErrorResponse> handleOrderPlacementFailure(OrderPlacementFailedException ex) {
-        log.error(ex.getMessage());
-        log.error(Arrays.toString(ex.getStackTrace()));
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse("InternalServerError", "Try again later."));
     }
 
     @ExceptionHandler(value = {StockUpdateFailedException.class})
     public ResponseEntity<ErrorResponse> handleStockUpdateRetrialsFailure(StockUpdateFailedException ex) {
-        log.error(ex.getMessage());
-        log.error(Arrays.toString(ex.getStackTrace()));
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse("InternalServerError", "Try again later."));
     }
 
     @ExceptionHandler(value = {UnauthorizedAccess.class})
     public ResponseEntity<ErrorResponse> handleUnAuthorizedAccess(UnauthorizedAccess ex) {
-        log.error(ex.getMessage());
-        log.error(Arrays.toString(ex.getStackTrace()));
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ErrorResponse("Unauthorized", "Not allowed."));
     }
 
